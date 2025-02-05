53437fb4e452b43f91932a458901fd59c81c1cc7
Avsesh-shakya <avseshshakya73@gmail.com>
2025-02-05 18:38:46 +0530

    s

---
+++ src/Components/DescriptionBox/DescriptionBox.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./DescriptionBox.css"
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+       <p> An e-commerce webside is an online platform that facilitate
+        buying and selling of products or services over the internet
+        serves as a virtural marketplace where businesses and individual
+        showcase their products, interact with customers, and conduct
+        transactions without the need for a physical presence,e-commerce
+        websides have gained immense popuarity due to their accessiblity, and the 
+        global reach they offer.</p>
+        <p>Buying and selling of products or services over the internet
+        serves</p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
+
--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrum/Breadcrum';
+import ProdutDisplay from '../Components/ProductDisplay/ProdutDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProduct from '../Components/RelatedProduct/RelatedProduct';
 
 
 const Product = () => {
@@ -12,6 +15,9 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product}/>
+      <ProdutDisplay product={product}/>
+      <DescriptionBox/>
+      <RelatedProduct/>
 
       
     </div>
